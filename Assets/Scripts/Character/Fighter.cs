82560e61be71482537ced701602de58bf3d99bfc
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-06-04 17:47:23 +0700

    Player

--- Assets/Scripts/Character/Fighter.cs
+++ Assets/Scripts/Character/Fighter.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public abstract class Fighter : MonoBehaviour
 {
+    [Header("Character Stats")]
     [SerializeField] protected float moveSpeed = 10f;
     [SerializeField] protected float Health = 100f;
 
+    [Header("Character Status")]
+    [SerializeField] protected bool isRunning;
+    [SerializeField] protected bool isDie;
+    [SerializeField] protected bool isFighting;
     protected virtual void TakeDamge(int damage)
     {
         if (Health <= 0)
         {
             Debug.Log("Over!!!");
+            Die();
             return;
         }
         Health -= damage;
     }
-    protected virtual void Die()
-    {
-
-    }
+    protected virtual void Die() { }
+    protected virtual void Attack() { }
+    
 
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,53 +1,114 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(PlayerInput))]
 public class Player : Fighter
 {
+    private Vector2 movementVector;
+    private Vector3 MoveDirection;
     private Animator animator;
     private CharacterController characterController;
-    private PlayerInput playerInput;
+    private InputSystem_Actions inputActions;
     private float movementX;
     private float movementY;
 
-    [SerializeField] bool isRunning;
-    [SerializeField] bool isFighting;
-    [SerializeField] bool isDie;
+    [Header("Player Combo System")]
+    [SerializeField] private int comboStep = 1;
+    [SerializeField] private float comboTime = 0.5f;
+    private float lastComboTime;
 
-    void Start()
+    void Awake()
     {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
-        playerInput = GetComponent<PlayerInput>();
+        inputActions = new InputSystem_Actions();
+
+        inputActions.Player.Move.performed += ctx =>
+        {
+            movementVector = ctx.ReadValue<Vector2>();
+            movementX = movementVector.x;
+            movementY = movementVector.y;
+            MoveDirection = new Vector3(movementX, 0f, movementY);
+        };
+
+        inputActions.Player.Move.canceled += ctx =>
+        {
+            movementVector = Vector2.zero;
+            MoveDirection = Vector3.zero;
+        };
+
+        inputActions.Player.Attack.performed += ctx => Attack();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
     }
 
     void Update()
     {
-        Vector3 inputDir = new Vector3(movementX, 0f, movementY);
+        PlayerControl();
+    }
 
-        if (inputDir.magnitude > 0.1f)
+    protected override void Attack()
+    {
+        if (Time.time > lastComboTime + comboTime)
         {
-            // Tính góc hướng nhập vào
-            float targetAngle = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            comboStep = 1;
+        }
+        else
+        {
+            comboStep++;
+            if (comboStep > 2) comboStep = 1;
+        }
 
-            // Di chuyển theo hướng nhân vật đã xoay
-            Vector3 moveDir = transform.forward; // luôn đi tới theo mặt của nhân vật
-            characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+        ComboAnimationControl(comboStep);
+        isFighting = true;
+        lastComboTime = Time.time;
+    }
+
+    void ComboAnimationControl(int comboAnimation)
+    {
+        switch (comboAnimation)
+        {
+            case 1:
+                animator.SetTrigger("PunchR");
+                break;
+            case 2:
+                animator.SetTrigger("PunchL");
+                break;
         }
     }
 
-
-    void OnMove(InputValue value)
+    void Moving()
     {
-        Vector2 movementVector = value.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-        isRunning = true;
-        isFighting = false;
-        isDie = false;
+        characterController.Move(MoveDirection * moveSpeed * Time.deltaTime);
+        animator.SetFloat("Speed", MoveDirection.magnitude);
+    }
+
+    void Rotating()
+    {
+        if (MoveDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = MathF.Atan2(MoveDirection.x, MoveDirection.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            Moving();
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+        }
+    }
+
+    void PlayerControl()
+    {
+        Rotating();
     }
 }
