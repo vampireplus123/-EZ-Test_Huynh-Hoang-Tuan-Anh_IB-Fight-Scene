158e9fb42743bbb5b3673330938f37f52c1fb1cd
Anh Huynh <66988477+vampireplus123@users.noreply.github.com>
2025-06-06 19:57:00 +0700

    updatr

--- Assets/Scripts/Character/Fighter.cs
+++ Assets/Scripts/Character/Fighter.cs
@@ -11,9 +11,47 @@ public abstract class Fighter : MonoBehaviour
     [SerializeField] protected bool isRunning;
     [SerializeField] public bool isDie;
     [SerializeField] protected bool isFighting;
+    [SerializeField] protected bool isWinning;
+    [SerializeField] protected bool isMoving;
+    [SerializeField] protected bool isAttacking;
+
+    public bool CanMove()
+    {
+        if (isDie)
+            return false;
+        if (isAttacking)
+            return false;
+        if (isWinning)
+            return false;
+        return true;
+    }
+    public bool CanAttack()
+    {
+        if (isDie)
+            return false;
+        if (isMoving)
+            return false;
+        if (isWinning)
+            return false;
+        return true;
+    }
+
+    public bool CanWin()
+    {
+        if (isDie)
+            return false;
+        if (isMoving)
+            return false;
+        if (isAttacking)
+            return false;
+        return true;
+    }
+
+
     public virtual void TakeDamge(int damage) { }
     public virtual void BeingHit() { }
     public virtual void Winner() { }
+
     public virtual int SetHealth(int NewHealth)
     {
         Health = NewHealth;
@@ -36,5 +74,4 @@ public abstract class Fighter : MonoBehaviour
     }
     protected virtual void Die() { }
     protected virtual void Attack() { }
-
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,4 @@
-using System;
+// Player.cs
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -15,6 +15,8 @@ public class Player : Fighter
     private float movementX;
     private float movementY;
 
+    [SerializeField] private float rotationSpeed = 10f;
+
     [Header("Player Combo System")]
     [SerializeField] private int comboStep = 1;
     [SerializeField] private float comboTime = 0.5f;
@@ -24,22 +26,31 @@ public class Player : Fighter
     {
         inputActions = new InputSystem_Actions();
     }
+
     void OnEnable()
     {
         inputActions.Player.Enable();
+
         inputActions.Player.Move.performed += context =>
         {
-            movementVector = context.ReadValue<Vector2>();
-            movementX = movementVector.x;
-            movementY = movementVector.y;
-            moveDirection = new Vector3(movementX, 0f, movementY).normalized;
+            if (!isAttacking) // Không cho di chuyển khi đang đánh
+            {
+                movementVector = context.ReadValue<Vector2>();
+                movementX = movementVector.x;
+                movementY = movementVector.y;
+                moveDirection = new Vector3(movementX, 0f, movementY).normalized;
+                isMoving = moveDirection.magnitude > 0.1f;
+            }
         };
 
         inputActions.Player.Move.canceled += context =>
         {
             movementVector = Vector2.zero;
             moveDirection = Vector3.zero;
+            isMoving = false;
         };
+
+        inputActions.Player.Attack.performed += ctx => Attack();
     }
 
     void Start()
@@ -50,10 +61,7 @@ public class Player : Fighter
 
         GameManager.Instance.RegisterFighter(this);
         LevelManager.Instance.RegisterFighter(this);
-
-        inputActions.Player.Attack.performed += ctx => Attack();
     }
-
 
     void OnDisable()
     {
@@ -65,13 +73,25 @@ public class Player : Fighter
         LevelManager.Instance.UnregisterFighter(this);
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        PlayerControl();
+        if (CanMove())
+        {
+            Moving();
+            Rotating();
+            isMoving = moveDirection.magnitude > 0.1f;
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0f);
+            isMoving = false;
+        }
     }
 
     protected override void Attack()
     {
+        if (!CanAttack()) return;
+
         if (Time.time > lastComboTime + comboTime)
         {
             comboStep = 1;
@@ -82,10 +102,18 @@ public class Player : Fighter
             if (comboStep > 2) comboStep = 1;
         }
 
+        isAttacking = true;
         ComboAnimationControl(comboStep);
         isFighting = true;
         lastComboTime = Time.time;
-        StartCoroutine(MoveWhenAfterCombo());
+        StartCoroutine(AfterAttackRoutine());
+    }
+
+    IEnumerator AfterAttackRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        isFighting = false;
+        isAttacking = false;
     }
 
     void ComboAnimationControl(int comboAnimation)
@@ -112,69 +140,67 @@ public class Player : Fighter
     {
         if (moveDirection.magnitude >= 0.1f)
         {
+            
+            if (isWinning)
+            {
+                isWinning = false;
+                animator.SetTrigger("isIdle"); 
+            }
+
+            
             float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
             Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-            float rotationSpeed = 10f;
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * rotationSpeed);
+
+            animator.SetFloat("Speed", moveDirection.magnitude);
         }
         else
         {
             animator.SetFloat("Speed", 0f);
         }
-    }
 
+    }
 
     protected override void Die()
     {
-        if (isDie) return;
+        if (isDie || isMoving || isAttacking || isWinning) return;
         isDie = true;
         StartCoroutine(DieCoroutine());
     }
+
     public override void TakeDamge(int damage)
     {
+        if (isDie || isMoving || isAttacking || isWinning) return;
+
+        Health -= damage;
         if (Health <= 0)
         {
             Die();
             Debug.Log("Over!!!");
             return;
         }
-        Health -= damage;
     }
+
     public override void BeingHit()
     {
-        animator.SetTrigger("BeingHit");
         Debug.Log("Enemy Hit toi");
+        animator.SetTrigger("BeingHit");
         Debug.Log(Health);
     }
 
     public override void Winner()
     {
+        if (!CanWin()) return;
+        isWinning = true;
         animator.SetTrigger("Win");
     }
-    void PlayerControl()
-    {
-        if (isFighting || isDie)
-        {
-            animator.SetFloat("Speed", 0f);
-            return;
-        }
-        Rotating();
-        Moving();
-    }
 
-    IEnumerator MoveWhenAfterCombo()
-    {
-        yield return new WaitForSeconds(1);
-        isFighting = false;
-    }
     IEnumerator DieCoroutine()
     {
         animator.SetTrigger("isKnockedOut");
         yield return new WaitForSeconds(2f);
-        gameObject.GetComponent<CapsuleCollider>().enabled = false;
-        gameObject.GetComponent<Player>().enabled = false;
+        GetComponent<CapsuleCollider>().enabled = false;
+        enabled = false;
         GameManager.Instance.OnFighterDeath(this);
     }
-
 }
